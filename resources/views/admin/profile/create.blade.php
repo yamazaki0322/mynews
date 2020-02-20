9356022f155b862928bec260016b70deb02c0464
yamazaki <yamasyatyo@gmail.com>
2020-02-20 11:08:01 +0000

    kadai13

--- resources/views/admin/profile/create.blade.php
+++ resources/views/admin/profile/create.blade.php
@@ -10,8 +10,39 @@
     <div class="container">
         <div class="row">
             <div class="col-md-8 mx-auto">
-                <h2>My プロフィール</h2>
+                <h2>プロフィール作成画面</h2>
             </div>
         </div>
+        <form action="{{ action('Admin\ProfileController@create') }}" method="post" enctype="multipart/form-data">
+        <div class="form-group row">
+                        <label class="col-md-2">氏名</label>
+                        
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" name="name" value="{{ old('title') }}">
+                        </div>
+        </div>
+        <div class="form-group row">    
+                      <label class="col-md-2">性別</label>
+                       <div class="col-md-8">
+                           <input type="radio" name="gender" value="male">男性
+                           <input type="radio" name="gender" value="female">女性
+                       </div>
+    　　</div>
+    　　 <div class="form-group row">
+                        <label class="col-md-2">趣味</label>
+                        
+                        <div class="col-md-4">
+                            <input type="text" class="form-control" name="hobby" value="{{ old('title') }}">
+                        </div>
+        </div>
+        <div class="form-group row">
+                        <label class="col-md-2">自己紹介欄</label>
+                        <div class="col-md-8">
+                            <textarea class="form-control" name="introduction" rows="20">{{ old('body') }}</textarea>
+                        </div>
+        </div>
+         <input type="submit" class="btn btn-primary" value="送信">
+         </form>
     </div>
+    
 @endsection
