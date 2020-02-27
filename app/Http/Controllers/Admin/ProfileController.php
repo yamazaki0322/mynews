b32e127d0bdce0dd654baf114f05ad2188072be6
yamazaki <yamasyatyo@gmail.com>
2020-02-27 14:28:28 +0000

    kadai16

--- app/Http/Controllers/Admin/ProfileController.php
+++ app/Http/Controllers/Admin/ProfileController.php
@@ -14,22 +14,30 @@ class ProfileController extends Controller
         return view('admin.profile.create');
     }
     
-    public function edit()
+    public function edit(Request $request)
     {
-        return view('admin.profile.edit');
+        $profile = Profile::find($request->id);
+        if (empty($profile)) {
+            abort(404);
+        }
+        return view('admin.profile.edit', ['profile_form' => $profile]);
     }
-    public function update()
+    public function update(Request $request)
     {
-        return redirect('admin/profile/edit');
+        $this->validate($request, Profile::$rules);
+        $profile = Profile::find($request->id);
+        $profile_form = $request->all();
+        unset($profile_form['token']);
+        return redirect('admin/profile/edit?id=1');
     }
     
     public function create(Request $request)
     {
-        $this->validate($request, profile::$rules);
-        $profile = new profile;
+        $this->validate($request, Profile::$rules);
+        $profile = new Profile;
         $form = $request->all();
         $profile->fill($form);
         $profile->save();
-        return redirect('admin.profile.create');
+        return redirect('admin/profile/create');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,17 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
-    Route::get('news/create', 'Admin\NewsController@add');
-    Route::post('news/create', 'Admin\NewsController@create');
+
+
+Route::group(['prefix' => 'admin'], function() {
+    Route::get('news/create', 'Admin\NewsController@add')->middleware('auth');
+    Route::post('news/create', 'Admin\NewsController@create')->middleware('auth');
+    Route::get('news', 'Admin\NewsController@index')->middleware('auth');
+    Route::get('news/edit', 'Admin\NewsController@edit')->middleware('auth');
+    Route::post('news\edit', 'Admin\NewsController@update')->middleware('auth');
+    Route::get('news\delete', 'Admin\NewsController@delete')->middleware('auth');
 });
+
 
 /* 課題３*/
 Route::get('XXX', 'AAAController@bbb');
@@ -28,7 +35,7 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
     Route::get('profile/create', 'Admin\ProfileController@add');
     Route::get('profile/edit', 'Admin\ProfileController@edit');
     Route::post('profile/create', 'Admin\ProfileController@create');
-    Route::post('profile/edit', 'Admin\profileController@update');
+    Route::post('profile/edit', 'Admin\ProfileController@update');
 });
 Auth::routes();
 
