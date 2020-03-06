39b0e64a2c87ee3800f55af266053a26233a8b5a
yamazaki <yamasyatyo@gmail.com>
2020-03-06 05:44:20 +0000

    kadai18

--- routes/web.php
+++ routes/web.php
@@ -40,3 +40,5 @@ Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/', 'NewsController@index');
